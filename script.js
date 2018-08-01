d3f27f5b8f2dc109bc05f84558e241ae96f3d34f
am1t <amit.w.gawande@gmail.com>
2018-08-01 21:06:47 +0530

    Added support for webmentions. + minor styling

--- script.js
+++ script.js
@@ -1,3 +1,76 @@
 {{{appJS}}}
 
+var post_url = window.location.href;
+
+$(document).ready(function(){	
+	$("ul#mentions-list").empty();
+  $.getJSON("https://webmention.io/api/mentions?jsonp=?", {
+    target: post_url
+  }, function(data){
+		var social_media_likes = "";
+		var social_media_repost = "";
+		var social_media_post = "";
+		if(data.links.length !== 0){
+			$("#post-mentions").show();
+		}
+  	$.each(data.links, function(i, v){
+  		var activity_type = data.links[i].activity.type;
+  		
+  		var men_content = "";
+  		if(activity_type && activity_type == "like"){
+  			if(!social_media_likes){
+  				social_media_likes = "<li class=\"mention-social\"><i class=\"fa fa-twitter\"></i> ";
+  			}
+  			social_media_likes = social_media_likes + 
+  			"<a href=\"" + data.links[i].data.url + "\">"
+				+ data.links[i].data.author.name + "</a>, ";
+  		} else if(activity_type && activity_type == "repost"){
+  			if(!social_media_repost){
+  				social_media_repost = "<li class=\"mention-social\"><i class=\"fa fa-twitter\"></i> ";
+  			}
+  			social_media_repost = social_media_repost + 
+  			"<a href=\"" + data.links[i].data.url + "\">"
+				+ data.links[i].data.author.name + "</a>, ";
+  		}else if(activity_type && activity_type == "link"){
+  			if(!social_media_post){
+  				social_media_post = "<li class=\"mention-social\"><i class=\"fa fa-link\"></i> ";
+  			}
+  			social_media_post = social_media_post + 
+  			"<a href=\"" + data.links[i].data.url + "\">"
+				+ data.links[i].data.author.name + "</a>, ";
+  		} else if(activity_type && activity_type == "reply"){
+					let mention_date = new Date(data.links[i].verified_date);
+	  			if(data.links[i].data.content) {
+					men_content = data.links[i].data.content;
+				}
+  			$("ul#mentions-list").prepend( "<li class=\"mention\">"
+				+ "<div class=\"mention-author\"><i class=\"fa fa-comments\"></i> <a href=\"" + data.links[i].data.author.url + "\">"
+				+ data.links[i].data.author.name + "</a> <span class=\"commented\">replied:</span></div>"
+				+ "<div class=\"mention-text\">" + men_content + "</div>"
+				+ "<div class=\"mention-date\"><a href=\"" + data.links[i].data.url + "\">"
+				+ "<time>" + mention_date.getUTCDate() + "/" + (mention_date.getUTCMonth() + 1) 
+				+ "/" + mention_date.getUTCFullYear()
+			  + "</time></a></div>"
+    		+ "</li>");
+    	}
+		});
+    if(social_media_post){
+			social_media_post = social_media_post.substr(0, social_media_post.length - 2);
+			social_media_post = social_media_post + " <span class=\"commented\">linked to this.</span></li>";
+			$("ul#mentions-list").prepend(social_media_post);
+		}			
+    if(social_media_repost){
+			social_media_repost = social_media_repost.substr(0, social_media_repost.length - 2);
+    	social_media_repost = social_media_repost + " <span class=\"commented\">reposted this on Twitter.</span></li>";
+	    $("ul#mentions-list").prepend(social_media_repost);
+		}
+		if(social_media_likes){
+			social_media_likes = social_media_likes.substr(0, social_media_likes.length - 2);
+			social_media_likes = social_media_likes  + " <span class=\"commented\">liked this on Twitter.</span></li>";
+			$("ul#mentions-list").prepend(social_media_likes);
+		}
+	});
+});
+
+
 // This script is embedded in the footer of every page
