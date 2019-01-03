bf61314038d0f09f846279ad6f1fc9c290b8515d
am1t <amit.w.gawande@gmail.com>
2019-01-03 12:03:57 +0530

    Stable Working Theme

--- script.js
+++ script.js
@@ -15,48 +15,51 @@ $(document).ready(function(){
 		}
   	$.each(data.links, function(i, v){
   		var activity_type = data.links[i].activity.type;
-  		
-  		var men_content = "";
-  		if(activity_type && activity_type == "like"){
-  			if(!social_media_likes){
-  				social_media_likes = "<li class=\"mention-social\"><i class=\"fa fa-thumbs-up\"></i> ";
-  			}
-  			social_media_likes = social_media_likes + 
-  			"<a href=\"" + data.links[i].data.url + "\">"
-				+ data.links[i].data.author.name + "</a>, ";
-  		} else if(activity_type && activity_type == "repost"){
-  			if(!social_media_repost){
-  				social_media_repost = "<li class=\"mention-social\"><i class=\"fa fa-twitter\"></i> ";
-  			}
-  			social_media_repost = social_media_repost + 
-  			"<a href=\"" + data.links[i].data.url + "\">"
-				+ data.links[i].data.author.name + "</a>, ";
-  		}else if(activity_type && activity_type == "link"){
-  			if(!social_media_post){
-  				social_media_post = "<li class=\"mention-social\"><i class=\"fa fa-link\"></i> ";
-  			}
-  			social_media_post = social_media_post + 
-  			"<a href=\"" + data.links[i].data.url + "\">"
-				+ data.links[i].data.author.name + "</a>, ";
-  		} else if(activity_type && activity_type == "reply"){
-					let mention_date = new Date(data.links[i].verified_date);
-	  			if(data.links[i].data.content) {
-					men_content = data.links[i].data.content;
+		  
+		if(data.links[i].data.author && data.links[i].data.author.name){
+			var men_content = "";
+			if(activity_type && activity_type == "like"){
+				if(!social_media_likes){
+					social_media_likes = "<li class=\"mention-social\"><i class=\"fa fa-thumbs-up\"></i> ";
 				}
-  			$("ul#mentions-list").prepend( "<li class=\"mention\">"
-				+ "<div class=\"mention-author u-author\">" 
-				+ "<img src=\"" + data.links[i].data.author.photo + "\" class=\"u-photo\"" 
-				+ "title=\"" + data.links[i].data.author.name + "\" width=\"40\" style=\"margin-right:10px;border-radius:4px;\">" 
-				+ "<a href=\"" + data.links[i].data.author.url + "\">"
-				+ data.links[i].data.author.name + "</a> <span class=\"commented\">replied:</span></div>"
-				+ "<div class=\"mention-text\">" + men_content + "</div>"
-				+ "<a href=\"" + data.links[i].data.url + "\" class=\"small\">"
-				+ "<time>" + mention_date.getUTCDate() + "/" + (mention_date.getUTCMonth() + 1) 
-				+ "/" + mention_date.getUTCFullYear()
-			  + "</time></a>"
-    		+ "</li>");
-    	}
-		});
+				social_media_likes = social_media_likes + 
+				"<a href=\"" + data.links[i].data.url + "\">"
+					+ data.links[i].data.author.name + "</a>, ";
+			} else if(activity_type && activity_type == "repost"){
+				if(!social_media_repost){
+					social_media_repost = "<li class=\"mention-social\"><i class=\"fa fa-twitter\"></i> ";
+				}
+				social_media_repost = social_media_repost + 
+				"<a href=\"" + data.links[i].data.url + "\">"
+					+ data.links[i].data.author.name + "</a>, ";
+			}else if(activity_type && activity_type == "link"){
+				if(!social_media_post){
+					social_media_post = "<li class=\"mention-social\"><i class=\"fa fa-link\"></i> ";
+				}
+				social_media_post = social_media_post + 
+				"<a href=\"" + data.links[i].data.url + "\">"
+					+ data.links[i].data.author.name + "</a>, ";
+			} else if(activity_type && activity_type == "reply"){
+						let mention_date = new Date(data.links[i].verified_date);
+					if(data.links[i].data.content) {
+						men_content = data.links[i].data.content;
+					}
+				$("ul#mentions-list").prepend( "<li class=\"mention\">"
+					+ "<div class=\"mention-author u-author\">" 
+					+ "<img src=\"" + data.links[i].data.author.photo + "\" class=\"u-photo\"" 
+					+ "title=\"" + data.links[i].data.author.name + "\" width=\"40\" style=\"margin-right:10px;border-radius:4px;\">" 
+					+ "<a href=\"" + data.links[i].data.author.url + "\">"
+					+ data.links[i].data.author.name + "</a> <span class=\"commented\">replied:</span></div>"
+					+ "<div class=\"mention-text\">" + men_content + "</div>"
+					+ "<a href=\"" + data.links[i].data.url + "\" class=\"small\">"
+					+ "<time>" + mention_date.getUTCDate() + "/" + (mention_date.getUTCMonth() + 1) 
+					+ "/" + mention_date.getUTCFullYear()
+				+ "</time></a>"
+				+ "</li>");
+			}
+		}
+  		
+	});
     if(social_media_post){
 			social_media_post = social_media_post.substr(0, social_media_post.length - 2);
 			social_media_post = social_media_post + " <span class=\"commented\">linked to this.</span></li>";
