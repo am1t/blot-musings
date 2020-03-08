b06411cd00812fc211624cad1f9aebdc55597e5e
Amit Gawande <amit.w.gawande@gmail.com>
2020-03-08 15:37:49 +0530

    Stable styling before migration

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 {{{appJS}}}
 
+
+const themeSwitch = document.querySelector('.theme-switcher button[type="button"]');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+}
+
+function switchTheme(e) {
+
+    if (localStorage.getItem('theme')) {
+		localStorage.removeItem('theme');
+		document.documentElement.removeAttribute('data-theme');
+    } else {
+		localStorage.setItem('theme', 'dark');
+		document.documentElement.setAttribute('data-theme', 'dark');
+    }
+
+    /*if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+    }
+    else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+	} */   
+}
+
+themeSwitch.addEventListener('click', switchTheme, false);
+
 var post_url = window.location.href;
 
 $(document).ready(function(){	
@@ -43,6 +73,13 @@ $(document).ready(function(){
 						let mention_date = new Date(data.links[i].verified_date);
 					if(data.links[i].data.content) {
 						men_content = data.links[i].data.content;
+						if(men_content.startsWith("<p>")){
+							men_content = men_content.split(/<p>(.+)/)[1];
+						}
+						while(men_content.startsWith("<a href=\"https://micro.blog")){
+							men_content = men_content.split(/<\/a>(.+)/)[1].trim();
+						}
+						men_content = "<p>" + men_content;
 					}
 				$("ul#mentions-list").prepend( "<li class=\"mention\">"
 					+ "<div class=\"mention-author u-author\">" 
